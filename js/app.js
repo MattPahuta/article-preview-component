7d037663227f4df259e81ac2db52e8d27570b9bd
Matt Pahuta <mhpahuta@gmail.com>
2022-10-10 15:12:41 -0700

    Update README, delete old code on app.js

--- js/app.js
+++ js/app.js
@@ -1,38 +1,3 @@
-
-// shareIcon.addEventListener('click', function() {
-
-//   // **** handle sharing styles for mobile layout ****
-
-//   if (cardShare.classList.contains('hidden')) { // if sharing is hidden
-//     // toggle hidden class for share/author info
-//     cardShare.classList.remove('hidden');
-//     authorInfo.classList.add('author-info__display');
-//     // change card footer bg color to dark
-//     cardFooter.classList.add('bg-footer__active');
-
-//     // toggle share icon bg color
-//     shareIcon.classList.add('bg-dark__share');
-//     shareIcon.classList.remove('bg-light__share');
-//     // toggle share arrow color
-//     shareArrow.classList.add('share-arrow-active')
-//     shareArrow.classList.remove('share-arrow-default')
-//   } else {
-//     // toggle hidden class for share/author info
-//     cardShare.classList.add('hidden');
-//     authorInfo.classList.remove('author-info__display');
-//     // remove dark bg from card footer
-//     cardFooter.classList.remove('bg-footer__active');
-
-//     // toggle share icon bg color
-//     shareIcon.classList.add('bg-light__share');
-//     shareIcon.classList.remove('bg-dark__share');
-//     // toggle share arrow color
-//     shareArrow.classList.remove('share-arrow-active');
-//     shareArrow.classList.add('share-arrow-default')
-//   }
-
-// })
-
 const shareIcon = document.querySelector('#share-icon');
 
 function handleShareMenu() {
