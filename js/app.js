ebc308a775c95b0b3ac6a438bca53393769d2106
Matt Pahuta <mhpahuta@gmail.com>
2022-10-09 17:15:23 -0700

    Refactor share interactivity styles on desktop

--- js/app.js
+++ js/app.js
@@ -1,19 +1,19 @@
-const shareIcon = document.querySelector('#share-icon');
-const cardFooter = document.querySelector('.card-footer');
-const authorInfo = document.querySelector('.author-info');
-const cardShare = document.querySelector('.card-share');
+const shareIcon = document.querySelector('#share-icon'); // need
+const cardFooter = document.querySelector('.card-footer'); // need
+const authorInfo = document.querySelector('.author-info'); // need
+const cardShare = document.querySelector('.card-share'); // need
 const shareArrow = document.querySelector('#share-arrow');
 
 shareIcon.addEventListener('click', function() {
 
   // **** handle sharing styles for mobile layout ****
-  
+
   if (cardShare.classList.contains('hidden')) { // if sharing is hidden
     // toggle hidden class for share/author info
     cardShare.classList.remove('hidden');
-    authorInfo.classList.add('hidden');
+    // authorInfo.classList.add('hidden');
     // change card footer bg color to dark
-    cardFooter.classList.add('bg-dark__footer');
+    cardFooter.classList.add('bg-footer__active');
 
     // toggle share icon bg color
     shareIcon.classList.add('bg-dark__share');
@@ -26,7 +26,7 @@ shareIcon.addEventListener('click', function() {
     cardShare.classList.add('hidden');
     authorInfo.classList.remove('hidden');
     // remove dark bg from card footer
-    cardFooter.classList.remove('bg-dark__footer');
+    cardFooter.classList.remove('bg-footer__active');
 
     // toggle share icon bg color
     shareIcon.classList.add('bg-light__share');
@@ -38,3 +38,10 @@ shareIcon.addEventListener('click', function() {
 
 })
 
+
+// shareIcon.addEventListener('click', () => {
+//   cardFooter.classList.toggle('card__footer--active');
+//   shareIcon.classList.toggle('card__share-icon--active');
+//   cardShare.classList.toggle('hidden');
+//   authorInfo.classList.toggle('hidden');
+// })
