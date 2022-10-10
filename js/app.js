74d26fb55e18b1879b4513dcb1732fac9007c20e
Matt Pahuta <mhpahuta@gmail.com>
2022-10-10 15:00:16 -0700

    Add final style adjustments, add custom variables

--- js/app.js
+++ js/app.js
@@ -1,12 +1,45 @@
-const shareIcon = document.querySelector('#share-icon'); // need
-const cardFooter = document.querySelector('.card-footer'); // need
-const authorInfo = document.querySelector('.author-info'); // need
-const cardShare = document.querySelector('.card-share'); // need
-const shareArrow = document.querySelector('#share-arrow');
 
-shareIcon.addEventListener('click', function() {
+// shareIcon.addEventListener('click', function() {
 
-  // **** handle sharing styles for mobile layout ****
+//   // **** handle sharing styles for mobile layout ****
+
+//   if (cardShare.classList.contains('hidden')) { // if sharing is hidden
+//     // toggle hidden class for share/author info
+//     cardShare.classList.remove('hidden');
+//     authorInfo.classList.add('author-info__display');
+//     // change card footer bg color to dark
+//     cardFooter.classList.add('bg-footer__active');
+
+//     // toggle share icon bg color
+//     shareIcon.classList.add('bg-dark__share');
+//     shareIcon.classList.remove('bg-light__share');
+//     // toggle share arrow color
+//     shareArrow.classList.add('share-arrow-active')
+//     shareArrow.classList.remove('share-arrow-default')
+//   } else {
+//     // toggle hidden class for share/author info
+//     cardShare.classList.add('hidden');
+//     authorInfo.classList.remove('author-info__display');
+//     // remove dark bg from card footer
+//     cardFooter.classList.remove('bg-footer__active');
+
+//     // toggle share icon bg color
+//     shareIcon.classList.add('bg-light__share');
+//     shareIcon.classList.remove('bg-dark__share');
+//     // toggle share arrow color
+//     shareArrow.classList.remove('share-arrow-active');
+//     shareArrow.classList.add('share-arrow-default')
+//   }
+
+// })
+
+const shareIcon = document.querySelector('#share-icon');
+
+function handleShareMenu() {
+  const cardFooter = document.querySelector('.card-footer'); 
+  const authorInfo = document.querySelector('.author-info'); 
+  const cardShare = document.querySelector('.card-share'); 
+  const shareArrow = document.querySelector('#share-arrow');
 
   if (cardShare.classList.contains('hidden')) { // if sharing is hidden
     // toggle hidden class for share/author info
@@ -14,7 +47,6 @@ shareIcon.addEventListener('click', function() {
     authorInfo.classList.add('author-info__display');
     // change card footer bg color to dark
     cardFooter.classList.add('bg-footer__active');
-
     // toggle share icon bg color
     shareIcon.classList.add('bg-dark__share');
     shareIcon.classList.remove('bg-light__share');
@@ -27,7 +59,6 @@ shareIcon.addEventListener('click', function() {
     authorInfo.classList.remove('author-info__display');
     // remove dark bg from card footer
     cardFooter.classList.remove('bg-footer__active');
-
     // toggle share icon bg color
     shareIcon.classList.add('bg-light__share');
     shareIcon.classList.remove('bg-dark__share');
@@ -35,5 +66,7 @@ shareIcon.addEventListener('click', function() {
     shareArrow.classList.remove('share-arrow-active');
     shareArrow.classList.add('share-arrow-default')
   }
+  
+}
 
-})
+shareIcon.addEventListener('click', handleShareMenu);
